bf4037bc30e92413cccba6be1f849e3cdd465e11
Karim Khasan <karimhasan3879@gmail.com>
2023-09-14 14:06:51 +0300

    feat: initialize the project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module ru.itmo.grafix {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
+    requires javafx.base;
+    requires javafx.swing;
+
+    opens ru.itmo.grafix to javafx.fxml;
+    exports ru.itmo.grafix;
+}
