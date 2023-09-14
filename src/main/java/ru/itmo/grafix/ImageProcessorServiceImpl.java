291682c733aed90751349d8ebe7f76ba398568fd
Karim Khasan <karimhasan3879@gmail.com>
2023-09-14 14:13:49 +0300

    style: reformat code

--- src/main/java/ru/itmo/grafix/ImageProcessorServiceImpl.java
+++ src/main/java/ru/itmo/grafix/ImageProcessorServiceImpl.java
@@ -1,6 +1,8 @@
 package ru.itmo.grafix;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,19 +14,19 @@ public class ImageProcessorServiceImpl implements ImageProcessorService {
         try (FileInputStream br = new FileInputStream(absolutePath)) {
             byte[] format = new byte[2];
             int count = br.read(format);
-            if(count != 2 || (char)format[0] != 'P' || ((char)format[1] != '5' && (char)format[1] != '6')) {
+            if (count != 2 || (char) format[0] != 'P' || ((char) format[1] != '5' && (char) format[1] != '6')) {
                 throw new RuntimeException("Unsupported image format");
             }
             int width = Integer.parseInt(readUntilWhitespace(br));
             int height = Integer.parseInt(readUntilWhitespace(br));
             int maxVal = Integer.parseInt(readUntilWhitespace(br));
             int bufSize = width * height;
-            if(format[1] == '6'){
+            if (format[1] == '6') {
                 bufSize *= 3;
             }
 
             byte[] buf = new byte[bufSize];
-            if(format[1] == '5'){
+            if (format[1] == '5') {
 //                for(int i = 0; i < width * height; ++i){
 //                    byte b = (byte) br.read();
 //                    buf[i * 3] = b;
@@ -32,8 +34,9 @@ public class ImageProcessorServiceImpl implements ImageProcessorService {
 //                    buf[i * 3 + 2] = b;
 //                }
                 br.read(buf);
+            } else {
+                br.read(buf);
             }
-            else{br.read(buf);}
 //            for (int i = 0; i < bufSize; ++i) {
 //                byte c = (byte) br.read();
 //                buf[i] = c;
@@ -47,11 +50,11 @@ public class ImageProcessorServiceImpl implements ImageProcessorService {
 
     private String readUntilWhitespace(InputStream br) throws IOException {
         char c = (char) br.read();
-        while(Character.isWhitespace(c)){
+        while (Character.isWhitespace(c)) {
             c = (char) br.read();
         }
         List<Character> buffer = new ArrayList<>();
-        while(!Character.isWhitespace(c)){
+        while (!Character.isWhitespace(c)) {
             buffer.add(c);
             c = (char) br.read();
         }
