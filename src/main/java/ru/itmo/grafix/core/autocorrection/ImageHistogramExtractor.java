db9da95b06f67ef03469db437a49ad7d26544c45
Karim Khasan <karimhasan3879@gmail.com>
2023-10-31 03:41:57 +0300

    feat: add image histogram display

---
+++ src/main/java/ru/itmo/grafix/core/autocorrection/ImageHistogramExtractor.java
@@ -0,0 +1,50 @@
+package ru.itmo.grafix.core.autocorrection;
+
+import ru.itmo.grafix.core.image.GrafixImage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImageHistogramExtractor {
+    public static List<ImageHistogramData> getImageHistogramData(GrafixImage image) {
+        int k = image.getTotalChannels();
+        int bytesPerPixel = k;
+        int offset = 0;
+        if (image.getChannel() != 0) {
+            k = 1;
+            offset = image.getChannel() - 1;
+        }
+        List<int[]> histData = new ArrayList<>(k);
+        for (int i = 0; i < k; ++i) {
+            histData.add(new int[256]);
+        }
+
+        float[] data = image.getData();
+        for (int i = 0; i < image.getHeight(); ++i) {
+            for (int j = 0; j < image.getWidth(); ++j) {
+                for (int c = 0; c < k; ++c) {
+                    int index = bytesPerPixel * (i * image.getWidth() + j) + c + offset;
+                    int pixelValue = (int)(data[index] * 255);
+                    ++histData.get(c)[pixelValue];
+                }
+            }
+        }
+
+        if (k != 1) {
+            int[] generalHistogramData = getGeneralHistogramData(histData);
+            histData.add(generalHistogramData);
+        }
+
+        return histData.stream().map(ImageHistogramData::new).toList();
+    }
+
+    private static int[] getGeneralHistogramData(List<int[]> histData) {
+        int[] generalHistogramData = new int[256];
+        for (int[] hist: histData) {
+            for (int i = 0; i < hist.length; ++i) {
+                generalHistogramData[i] += hist[i];
+            }
+        }
+        return generalHistogramData;
+    }
+}
--- src/main/java/ru/itmo/grafix/ui/components/windows/HistogramWindow.java
+++ src/main/java/ru/itmo/grafix/ui/components/windows/HistogramWindow.java
@@ -1,9 +1,37 @@
 package ru.itmo.grafix.ui.components.windows;
 
+import javafx.scene.Scene;
+import javafx.scene.chart.*;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import ru.itmo.grafix.core.autocorrection.ImageHistogramData;
 
-public class HistogramWindow extends VBox {
-    public HistogramWindow() {
+import java.util.List;
 
+public class HistogramWindow extends Stage {
+    public HistogramWindow(List<ImageHistogramData> histData) {
+        initModality(Modality.NONE);
+        VBox histList = new VBox();
+        Axis<Number> x = new NumberAxis(0, histData.get(0).getData().length, 1);
+        x.setTickLabelsVisible(false);
+        for (ImageHistogramData hist : histData) {
+            Axis<Number> y = new NumberAxis();
+            y.setTickLabelsVisible(false);
+            LineChart<Number, Number> barChart = new LineChart<>(x, y);
+            XYChart.Series<Number, Number> series = new XYChart.Series<>();
+            for (int i = 0; i < hist.getData().length; ++i) {
+                series.getData().add(new XYChart.Data<>(i, 0));
+                series.getData().add(new XYChart.Data<>(i, hist.getData()[i]));
+                series.getData().add(new XYChart.Data<>(i, 0));
+            }
+            barChart.getData().add(series);
+            barChart.setCreateSymbols(false);
+            barChart.setTitle("ABOBA");
+
+            histList.getChildren().add(barChart);
+        }
+
+        setScene(new Scene(histList, 300, 600));
     }
 }
