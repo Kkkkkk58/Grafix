03840a87a77464b420764c59210d4fe2eb6b56ac
Karim Khasan <karimhasan3879@gmail.com>
2023-11-27 00:51:00 +0300

    feat: add reversing filter [FIXME]

--- src/main/java/ru/itmo/grafix/core/imageprocessing/ImageProcessorServiceImpl.java
+++ src/main/java/ru/itmo/grafix/core/imageprocessing/ImageProcessorServiceImpl.java
@@ -8,6 +8,7 @@ import ru.itmo.grafix.core.exception.UnsupportedImageFormatException;
 import ru.itmo.grafix.core.image.GrafixImage;
 
 import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,9 +18,12 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.*;
+import java.util.zip.DataFormatException;
 import java.util.zip.Inflater;
 
 public class ImageProcessorServiceImpl implements ImageProcessorService {
+
+    private static final int PNG_GAMMA_DIVISOR = 100000;
     @Override
     public GrafixImage open(String absolutePath, ColorSpace colorSpace) {
         try (FileInputStream br = new FileInputStream(absolutePath)) {
@@ -39,7 +43,8 @@ public class ImageProcessorServiceImpl implements ImageProcessorService {
     @Override
     public ByteArrayOutputStream write(GrafixImage image) {
         image = ChannelDecomposer.getDecomposedImage(image);
-        String header = image.getFormat() + " " + image.getWidth() + " " + image.getHeight() + " " + image.getMaxVal() + " ";
+        String header =
+                image.getFormat() + " " + image.getWidth() + " " + image.getHeight() + " " + image.getMaxVal() + " ";
         try (ByteArrayOutputStream stream = new ByteArrayOutputStream(header.length() + image.getData().length + 1)) {
             stream.write(header.getBytes());
             stream.write(FbConverter.convertFloatToByte(image.getData()));
@@ -80,7 +85,8 @@ public class ImageProcessorServiceImpl implements ImageProcessorService {
             }
             byte[] buf = new byte[bufSize];
             br.read(buf);
-            return new GrafixImage(new String(format), width, height, 255, FbConverter.convertBytesToFloat(buf, maxVal), absolutePath, headerSize, new RGB());
+            return new GrafixImage(new String(format), width, height, 255, FbConverter.convertBytesToFloat(buf,
+                    maxVal), absolutePath, headerSize, new RGB());
         } catch (IOException inputException) {
             throw new ByteReaderException();
         }
@@ -88,7 +94,7 @@ public class ImageProcessorServiceImpl implements ImageProcessorService {
 
     private GrafixImage openPng(String absolutePath, FileInputStream br) {
         int headerInfoSize = 4;
-        try (ByteArrayOutputStream stream = new ByteArrayOutputStream()){
+        try (Decompressor decompressor = new Decompressor()) {
             IHDR params = null;
             byte[] format = new byte[6];
             int count = br.read(format);
@@ -98,7 +104,8 @@ public class ImageProcessorServiceImpl implements ImageProcessorService {
             }
             String type = "IHDR";
             Map<Integer, byte[]> plt = null;
-            while(!Objects.equals(type, "IEND")) {
+            float gamma = 1f;
+            while (!Objects.equals(type, "IEND")) {
                 byte[] headerInfoBuffer = new byte[headerInfoSize];
                 count = br.read(headerInfoBuffer);
                 int length = ByteBuffer.wrap(headerInfoBuffer).getInt();
@@ -107,35 +114,39 @@ public class ImageProcessorServiceImpl implements ImageProcessorService {
                 byte[] buffer = new byte[length];
                 count = br.read(buffer);
                 count = br.read(headerInfoBuffer);
-                if(Objects.equals(type, "IHDR")){
+                if (Objects.equals(type, "IHDR")) {
                     params = readIHDR(buffer);
                 } else if (Objects.equals(type, "PLTE")) {
-                    if(length % 3 != 0 || length > 256 * 3 || params == null || params.getColorType() == 0){
+                    if (length % 3 != 0 || length > 256 * 3 || params == null || params.getColorType() == 0) {
                         throw new UnsupportedImageFormatException();
                     }
                     plt = processPalette(buffer);
                 } else if (Objects.equals(type, "IDAT")) {
-                    if(params == null){
+                    if (params == null) {
                         throw new UnsupportedImageFormatException();
                     }
-                    byte[] d = readIDAT(buffer, params.isCompression(), plt);
-                    if(d != null) {
-                        stream.write(d);
-                    }
-                    else{
-                        stream.write(new byte[buffer.length]);
-                    }
+                    decompressor.addData(buffer);
                 } else if (Objects.equals(type, "gAMA")) {
-
+                    gamma = readGAMMA(buffer);
                 }
             }
-            return new GrafixImage("PNG", params.getWidth(), params.getHeight(), 255, FbConverter.convertBytesToFloat(stream.toByteArray(), 255), absolutePath, 0, new RGB());
+
+            if (params == null) {
+                throw new UnsupportedImageFormatException();
+            }
+            byte[] decoded = decompressor.decode();
+            byte[] imageData = getImageData(decoded, params.getWidth(), params.getHeight(), params.getColorType(), plt);
+
+            GrafixImage image = new GrafixImage("PNG", params.getWidth(), params.getHeight(), 255,
+                    FbConverter.convertBytesToFloat(imageData, 255), absolutePath, 0, new RGB());
+            image.setGamma(gamma);
+            return image;
         } catch (IOException inputException) {
             throw new ByteReaderException();
         }
     }
 
-    private IHDR readIHDR(byte[] chunk){
+    private IHDR readIHDR(byte[] chunk) {
         int width = ByteBuffer.wrap(chunk, 0, 4).getInt();
         int height = ByteBuffer.wrap(chunk, 4, 4).getInt();
         int depth = (chunk[8] & 0xff);
@@ -144,58 +155,140 @@ public class ImageProcessorServiceImpl implements ImageProcessorService {
         return new IHDR(width, height, depth, colorType, compression != 0);
     }
 
-    private byte[] readIDAT(byte[] chunk, boolean compression, Map<Integer, byte[]> plt){
-        chunk = decompressData(chunk);
-        if(plt == null){
-            return chunk;
-        }
-        //TODO: grayscale
-        byte[] newChunk = new byte[chunk.length * 3];
-        for(int i = 0; i < newChunk.length; i += 3){
-            newChunk[i] = plt.get((int) chunk[i / 3])[0];
-            newChunk[i + 1] = plt.get((int) chunk[i / 3])[1];
-            newChunk[i + 2] = plt.get((int) chunk[i / 3])[2];
-        }
-        return newChunk;
+    private float readGAMMA(byte[] buffer) {
+        return (float) ByteBuffer.wrap(buffer).getInt() / PNG_GAMMA_DIVISOR;
     }
 
-    private Map<Integer, byte[]> processPalette(byte[] palette){
+    private Map<Integer, byte[]> processPalette(byte[] palette) {
         Map<Integer, byte[]> plt = new HashMap<>(palette.length);
         int j = 0;
-        for(int i = 0; i < palette.length; ++i){
-            plt.put(i, new byte[]{palette[j], palette[j+1], palette[j+2]});
+        for (int i = 0; i < palette.length; ++i) {
+            plt.put(i, new byte[]{palette[j], palette[j + 1], palette[j + 2]});
             j += 3;
         }
         return plt;
     }
 
-    private byte[] decompressData(byte[] data){
-        byte[] output = new byte[0];
-
-        Inflater decompresser = new Inflater();
-        decompresser.reset();
-        decompresser.setInput(data);
-
-        ByteArrayOutputStream o = new ByteArrayOutputStream(data.length);
-        try {
-            byte[] buf = new byte[1024];
-            while (!decompresser.finished()) {
-                int i = decompresser.inflate(buf);
-                o.write(buf, 0, i);
-            }
-            output = o.toByteArray();
-        } catch (Exception e) {
-            output = data;
-            e.printStackTrace();
-        } finally {
-            try {
-                o.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+    private byte[] getImageData(byte[] decoded, int width, int height, int colorType, Map<Integer, byte[]> plt) {
+        // TODO refactor
+        // FIXME
+        int bytesPerPixel = (colorType == 0) ? 1 : 3;
+        byte[] data = new byte[width * height * bytesPerPixel];
+        int i = 0;
+        for (int scanline = 0; scanline < height; ++scanline) {
+            int filterType = decoded[bytesPerPixel * scanline * width + scanline] & 0xff;
+            for (int pixel = 0; pixel < width; ++pixel) {
+                if (plt == null) {
+                    for (int channel = 0; channel < bytesPerPixel; ++channel) {
+                        data[i + channel] = getReverseFilteredValue(decoded, data, filterType, scanline, pixel, channel, width, bytesPerPixel);
+                    }
+                } else {
+                    int paletteColor = decoded[getPngValueIndex(scanline, pixel, 0, width, bytesPerPixel)] & 0xff;
+                    byte[] color = plt.get(paletteColor);
+                    System.arraycopy(color, 0, data, i, bytesPerPixel);
+                }
+                i += bytesPerPixel;
             }
         }
 
-        decompresser.end();
-        return output;
+        return data;
+    }
+
+    private byte getReverseFilteredValue(byte[] decoded, byte[] reversed, int filterType, int scanline, int pixel, int channel, int width, int bytesPerPixel) {
+        byte value = decoded[getPngValueIndex(scanline, pixel, channel, width, bytesPerPixel)];
+
+        // TODO refactor, separate filters into classes
+        return switch (filterType) {
+            case 1 -> getReverseSubFilterValue(value, reversed, scanline, pixel, channel, width, bytesPerPixel);
+            case 2 -> getReverseUpFilterValue(value, reversed, scanline, pixel, channel, width, bytesPerPixel);
+            case 3 -> getReverseAverageFilterValue(value, reversed, scanline, pixel, channel, width, bytesPerPixel);
+            case 4 -> getReversePaethFilterValue(value, reversed, scanline, pixel, channel, width, bytesPerPixel);
+            default -> value;
+        };
+    }
+
+    private byte getReversePaethFilterValue(byte value, byte[] reversed, int scanline, int pixel, int channel, int width, int bytesPerPixel) {
+        // TODO check types (bytes or ints)
+        int paeth = (scanline == 0 || pixel == 0) ? 0 :
+                getPaethValue(reversed[getRgbValueIndex(scanline, pixel - 1, channel, width, bytesPerPixel)] & 0xff,
+                        reversed[getRgbValueIndex(scanline - 1, pixel, channel, width, bytesPerPixel)] & 0xff,
+                        reversed[getRgbValueIndex(scanline - 1, pixel - 1, channel, width, bytesPerPixel)] & 0xff);
+
+        return (byte) (((value & 0xff) + paeth) % 256);
+    }
+
+    private int getPaethValue(int a, int b, int c) {
+        int p = a + b - c;
+        int pa = Math.abs(p - a);
+        int pb = Math.abs(p - b);
+        int pc = Math.abs(p - c);
+        if (pa <= pb && pa <= pc) {
+            return a;
+        } else if (pb <= pc) {
+            return b;
+        }
+        return c;
+    }
+
+    private byte getReverseAverageFilterValue(byte value, byte[] reversed, int scanline, int pixel, int channel, int width, int bytesPerPixel) {
+        int average = (scanline == 0 || pixel == 0) ? 0 :
+                (((reversed[getRgbValueIndex(scanline, pixel - 1, channel, width, bytesPerPixel)] & 0xff)
+                        + (reversed[getRgbValueIndex(scanline - 1, pixel, channel, width, bytesPerPixel)] & 0xff)) / 2);
+        return (byte) (((value & 0xff) + average) % 256);
+    }
+
+    private byte getReverseUpFilterValue(byte value, byte[] reversed, int scanline, int pixel, int channel, int width, int bytesPerPixel) {
+        int up = (scanline == 0) ? 0 : reversed[getRgbValueIndex(scanline - 1, pixel, channel, width, bytesPerPixel)] & 0xff;
+        return (byte) (((value & 0xff) & 0xff + up) % 256);
+    }
+
+    private byte getReverseSubFilterValue(byte value, byte[] reversed, int scanline, int pixel, int channel, int width, int bytesPerPixel) {
+        int sub = (pixel == 0) ? 0 : reversed[getRgbValueIndex(scanline, pixel - 1, channel, width, bytesPerPixel)] & 0xff;
+        return (byte) (((value & 0xff) + sub) % 256);
+    }
+
+    private int getPngValueIndex(int row, int col, int channel, int width, int bytesPerPixel) {
+        return getRgbValueIndex(row, col, channel, width, bytesPerPixel) + row + 1;
+    }
+
+    private int getRgbValueIndex(int row, int col, int channel, int width, int bytesPerPixel) {
+        return bytesPerPixel * (row * width + col) + channel;
+    }
+
+
+    // TODO remove from this class
+    private static class Decompressor implements Closeable {
+        private final ByteArrayOutputStream baos;
+        private final Inflater inflater;
+
+        public Decompressor() {
+            this.baos = new ByteArrayOutputStream();
+            this.inflater = new Inflater();
+        }
+
+        public void addData(byte[] data) throws IOException {
+            baos.write(data);
+        }
+
+        public byte[] decode() {
+            try (ByteArrayOutputStream out = new ByteArrayOutputStream(baos.size())) {
+                inflater.setInput(baos.toByteArray());
+                byte[] buf = new byte[1024];
+                while (!inflater.finished()) {
+                    int i = inflater.inflate(buf);
+                    out.write(buf, 0, i);
+                }
+
+                return out.toByteArray();
+            } catch (IOException | DataFormatException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public void close() throws IOException {
+            baos.close();
+            inflater.end();
+        }
     }
 }
