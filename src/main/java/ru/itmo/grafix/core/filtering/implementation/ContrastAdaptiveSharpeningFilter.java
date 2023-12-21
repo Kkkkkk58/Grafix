34e06ea44343ce5c46fdbe25b9bf57820cbaf94e
Karim Khasan <karimhasan3879@gmail.com>
2023-12-21 20:42:33 +0300

    feat: implement CAS

--- src/main/java/ru/itmo/grafix/core/filtering/implementation/ContrastAdaptiveSharpeningFilter.java
+++ src/main/java/ru/itmo/grafix/core/filtering/implementation/ContrastAdaptiveSharpeningFilter.java
@@ -1,13 +1,19 @@
 package ru.itmo.grafix.core.filtering.implementation;
 
-import ru.itmo.grafix.core.filtering.Filter;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+import ru.itmo.grafix.core.filtering.ConvolutionalFilter;
 import ru.itmo.grafix.core.filtering.FilterType;
-import ru.itmo.grafix.core.image.GrafixImage;
+import ru.itmo.grafix.core.imageprocessing.PixelValueNormalizer;
 import ru.itmo.grafix.ui.components.dialogs.filters.SharpnessChoiceDialog;
 
-public class ContrastAdaptiveSharpeningFilter extends Filter {
-    private double sharpness;
+public class ContrastAdaptiveSharpeningFilter extends ConvolutionalFilter {
+    private static final int[][] CROSS_DXDY = {{1, 0}, {0, 1}, {1, 1}, {2, 1}, {1, 2}};
+    private static final int[][] DIAGONALS_DXDY = {{0, 0}, {2, 0}, {0, 2}, {2, 2}};
+    private static final int CAS_RADIUS = 1;
 
+    private double sharpness;
     public ContrastAdaptiveSharpeningFilter() {
         super(FilterType.CONTRAST_ADAPTIVE_SHARPENING);
     }
@@ -23,15 +29,35 @@ public class ContrastAdaptiveSharpeningFilter extends Filter {
     @Override
     public boolean setParams() {
         Double s = SharpnessChoiceDialog.getSharpnessInput();
-        if(s == null){
+        if (s == null) {
             return false;
         }
         sharpness = s;
+        setRadius(CAS_RADIUS);
         return true;
     }
 
     @Override
-    public GrafixImage apply(GrafixImage image) {
-        return null;
+    protected float applyInternal(float[] data) {
+        double[] crossValues = Arrays.stream(CROSS_DXDY).mapToDouble(dxdy -> data[dxdy[1] * getDiameter() + dxdy[0]]).toArray();
+        float max = (float) Arrays.stream(crossValues).max().orElse(0.0);
+        float min = (float) Arrays.stream(crossValues).min().orElse(0.0);
+
+        double[] diagonalValues = Arrays.stream(DIAGONALS_DXDY).mapToDouble(dxdy -> data[dxdy[1] * getDiameter() + dxdy[0]]).toArray();
+        float diagMax = (float) Arrays.stream(diagonalValues).max().orElse(0.0);
+        float diagMin = (float) Arrays.stream(diagonalValues).min().orElse(0.0);
+
+        max += diagMax;
+        min += diagMin;
+
+        double invertedMax = 1.0 / max;
+        double amp = Math.sqrt(invertedMax * Math.min(min, 2 - max));
+        double w = -amp * (sharpness * (1.0 / 5.0 - 1.0 / 8.0) + 1.0 / 8.0);
+        double[] kernel = {0, w, 0,
+                            w, 1, w,
+                            0, w, 0};
+
+        float value = (float) (IntStream.range(0, data.length).mapToDouble(i -> kernel[i] * data[i]).sum() / (1 + 4.0 * w));
+        return PixelValueNormalizer.normalize(value);
     }
 }
