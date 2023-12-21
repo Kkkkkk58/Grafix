993a5daa2d39ea2a1a21a5f10d3c511fcb52311f
Karim Khasan <karimhasan3879@gmail.com>
2023-12-21 15:35:18 +0300

    feat: implement sobel filter

--- src/main/java/ru/itmo/grafix/core/filtering/implementation/SobelFilter.java
+++ src/main/java/ru/itmo/grafix/core/filtering/implementation/SobelFilter.java
@@ -1,11 +1,19 @@
 package ru.itmo.grafix.core.filtering.implementation;
 
-import ru.itmo.grafix.core.filtering.Filter;
+import java.util.stream.IntStream;
+
+import ru.itmo.grafix.core.filtering.ConvolutionalFilter;
 import ru.itmo.grafix.core.filtering.FilterType;
-import ru.itmo.grafix.core.image.GrafixImage;
+import ru.itmo.grafix.core.imageprocessing.PixelValueNormalizer;
 
-public class SobelFilter extends Filter {
-
+public class SobelFilter extends ConvolutionalFilter {
+    private static final int SOBEL_RADIUS = 1;
+    private static final float[] MASK_X = {-1, -2, -1,
+                                            0, 0, 0,
+                                            1, 2, 1};
+    private static final float[] MASK_Y = {-1, 0, 1,
+                                            -2, 0, 2,
+                                            -1, 0, 1};
 
     public SobelFilter() {
         super(FilterType.SOBEL);
@@ -13,11 +21,16 @@ public class SobelFilter extends Filter {
 
     @Override
     public boolean setParams() {
+        setRadius(SOBEL_RADIUS);
         return true;
     }
 
     @Override
-    public GrafixImage apply(GrafixImage image) {
-        return null;
+    protected float applyInternal(float[] data) {
+        double gradientX = IntStream.range(0, data.length).mapToDouble(i -> data[i] * MASK_X[i]).sum();
+        double gradientY = IntStream.range(0, data.length).mapToDouble(i -> data[i] * MASK_Y[i]).sum();
+
+        // FIXME 3-channel image
+        return PixelValueNormalizer.normalize((float) Math.sqrt(gradientX * gradientX + gradientY * gradientY));
     }
 }
