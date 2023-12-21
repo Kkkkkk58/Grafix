066effb872ba5f0b65ec8e4d312e0c8fffbaae26
Karim Khasan <karimhasan3879@gmail.com>
2023-12-22 00:32:13 +0300

    fix: fix unsharp masking

--- src/main/java/ru/itmo/grafix/core/filtering/implementation/UnsharpMaskingFilter.java
+++ src/main/java/ru/itmo/grafix/core/filtering/implementation/UnsharpMaskingFilter.java
@@ -2,6 +2,7 @@ package ru.itmo.grafix.core.filtering.implementation;
 
 import ru.itmo.grafix.core.filtering.ConvolutionalFilter;
 import ru.itmo.grafix.core.filtering.FilterType;
+import ru.itmo.grafix.core.imageprocessing.PixelValueNormalizer;
 import ru.itmo.grafix.ui.components.dialogs.filters.UnsharpMaskingParamsDialog;
 import ru.itmo.grafix.ui.models.UnsharpMaskingParams;
 
@@ -34,8 +35,7 @@ public class UnsharpMaskingFilter extends ConvolutionalFilter {
         float originalValue = data[data.length / 2];
         float difference = originalValue - smoothed;
 
-        // FIXME normalize amount
-        return (float) (originalValue +
-                ((Math.abs(difference) >= params.getThreshold()) ? params.getAmount() * difference : 0));
+        return PixelValueNormalizer.normalize((float) (originalValue +
+                ((Math.abs(difference) >= params.getThreshold() / 255.0) ? params.getAmount() * difference : 0)));
     }
 }
